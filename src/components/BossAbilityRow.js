54fbe3186ed39d57905ba35ffc79a49fb814f859
Rgorn <201508933@post.au.dk>
2023-07-07 00:21:30 +0200

    fixed timeline display order

--- src/components/BossAbilityRow.js
+++ src/components/BossAbilityRow.js
@@ -2,15 +2,14 @@ import React from "react";
 import WowheadIcon from "./WowheadIcon";
 import "../CSSFiles/bossAbilityRow.css";
 
-
-function BossAbility({ ability, pixelsPerSecond }) {
+function BossAbilityRow({ ability, pixelsPerSecond }) {
 	const leftPosition = ability.time * pixelsPerSecond;
 
 	return (
-		<div className="bossAbilityRow" style={{ left: `${leftPosition}px` }}>
+		<div className="bossAbility" style={{ left: `${leftPosition}px` }}>
 			<WowheadIcon name={ability.icon} size="30px" />
 		</div>
 	);
 }
 
-export default BossAbility;
+export default BossAbilityRow;
