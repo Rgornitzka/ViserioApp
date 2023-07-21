0f1134990fe8563b4f2fac05ecd806b5da0cdc25
Rgorn <201508933@post.au.dk>
2023-07-21 22:49:58 +0200

    fixed context so it gives more context. fuck.

--- src/components/Timeline/Timeline/Timeline.js
+++ src/components/Timeline/Timeline/Timeline.js
@@ -1,55 +1,25 @@
-import React, {
-	createContext,
-	useContext,
-	useEffect,
-	useRef,
-} from "react";
+// Timeline.js
+import React, { useContext, useEffect } from "react";
 import TimeGrid from "./TimeGrid";
-import useStore from "../../../store";
+import { TimelineContext } from "../../../App";
 import "../../../CSSFiles/Timeline/Timeline.css";
 
-// Create the context
-const TimelineContext = createContext();
-
-// Create a custom hook for accessing the context
-export function useTimeline() {
-	const context = useContext(TimelineContext);
-	if (!context) {
-		throw new Error("useTimeline must be used within a TimelineProvider");
-	}
-	return context;
-}
-
-/**
- * Component representing a timeline.
- */
 function Timeline() {
-	const { setPanelRef, setPanelWidth } = useStore();
-  const panelRef = useRef(null);
-
+	const { panelRef, setPanelWidth } = useContext(TimelineContext);
 
 	// Get panel width when it is available
 	useEffect(() => {
 		if (panelRef.current) {
-			setPanelRef(panelRef.current);
 			setPanelWidth(panelRef.current.offsetWidth);
 		}
-	}, [panelRef]);
-
-	const value = {
-		panelRef,
-		setPanelRef,
-		setPanelWidth,
-	};
+	}, [panelRef, setPanelWidth]);
 
 	return (
-		<TimelineContext.Provider value={value}>
-			<div className="timeline-container">
-				<div ref={panelRef} className="timeline-panel">
-					<TimeGrid />
-				</div>
+		<div className="timeline-container">
+			<div ref={panelRef} className="timeline-panel">
+				<TimeGrid />
 			</div>
-		</TimelineContext.Provider>
+		</div>
 	);
 }
 
